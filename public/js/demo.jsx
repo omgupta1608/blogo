1625eedd33f8faa1ef88cbdac1d1b24fe227b7a7
Om Gupta <omgupta1608@gmail.com>
2020-07-28 22:32:17 +0530

    some ui work

---
+++ public/js/demo.jsx
@@ -0,0 +1,51 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("https://api.example.com/items")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result.items
+            });
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+  
+    render() {
+      const { error, isLoaded, golu } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+          <ul>
+            {golu.map(item => (
+              <li key={golu.name}>
+                {golu.name} {golu.price}
+              </li>
+            ))}
+          </ul>
+        );
+      }
+    }
+  }
