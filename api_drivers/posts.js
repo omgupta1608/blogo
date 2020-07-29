9413e3052caeeefa75fcb187fa7d85cb23db0b67
Om Gupta <omgupta1608@gmail.com>
2020-07-29 14:07:09 +0530

    image upload initiated

--- api_drivers/posts.js
+++ api_drivers/posts.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var multer = require('multer');
+const helpers = require('../helpers/upload_helper');
 
 var router = express.Router();
 
 var sql = require('mysql');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
 
 const connection = sql.createConnection({
     host: 'localhost',
@@ -27,11 +40,12 @@ router.get('/all_posts', (req,res) => {
 
 });
 
-router.post('/:content/:date', (req, res) => {
+router.post('/:content/:date/:path', (req, res) => {
     var content = req.params.content;
     var date = req.params.date;
     var likes = 0;
-    var sql = 'INSERT INTO posts (date_time,content,likes_count) values ("' + date + '","' + content + '",' + likes + ')';
+    var path = req.params.path;
+    var sql = 'INSERT INTO posts (date_time,content,likes_count,imageUrl) values ("' + date + '","' + content + '",' + likes + ',"' + path + '")';
 
     connection.query(sql, (err,data) => {
         if(err) throw err;
@@ -42,5 +56,33 @@ router.post('/:content/:date', (req, res) => {
       });
       
 })
+
+router.post('/upload_image' ,(req, res) => {
+    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('profile_pic');
+
+    upload(req, res, function(err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send({
+            code: 200,
+            filePath: req.file.path
+        });
+    });
+});
 
 module.exports.router = router;
--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,15 +1,43 @@
 const ENV_HOST =  'http://'+window.location.hostname+':4000';
+var current_image = null;
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            current_image = e.target.result;
+            $('#your_image').attr('src', e.target.result).attr('height', 200).attr('width', 200);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
 
 function addPost(){
     //window.alert("POST ADDED");
     var content = document.getElementById("postContent").value.toString();
     //if(content != ""){
-        console.log(content);
-    add_to_db(content);
+    var image = document.getElementById("uploaded_image").value;
+    var file = uploadImage(image);
+    console.log(content);
+    add_to_db(content, current_image);
     //}
 }
 
-function add_to_db(content){
+async function uploadImage(image){
+    await $.ajax({
+        type: 'POST',
+        dataType: "multipart/form-data",
+        data: image,
+        url: ENV_HOST + 'api/posts/upload_image',
+        success: function(data){
+            return data.filePath;
+        }
+    })
+}
+
+
+function add_to_db(content, filePath){
 
     var d = new Date();
     var month = d.getMonth() + 1;
@@ -17,7 +45,7 @@ function add_to_db(content){
 
     $.ajax({
         type: 'POST',
-        url: ENV_HOST + '/api/posts/' + content + '/' + date,
+        url: ENV_HOST + '/api/posts/' + content + '/' + date + '/' + filePath,
         dataType: "json",
         success: function(data){
             
