f486afef89dd6d223ca9e4f2a0e161770facffcd
Om Gupta <omgupta1608@gmail.com>
2020-07-31 02:24:29 +0530

    image upload code done

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -16,37 +16,50 @@ function readURL(input) {
 function addPost(){
     //window.alert("POST ADDED");
     var content = document.getElementById("postContent").value.toString();
+
+    console.log($('#uploaded_image')[0].files[0]);
+    var formdata = new FormData();
+    formdata.append('post_image', $('#uploaded_image')[0].files[0]);
+    formdata.append('post_content', $('#postContent').val().toString());
+
+    add_to_db(formdata, $('#postContent').val().toString());
+    
     //if(content != ""){
-    var image = document.getElementById("uploaded_image").value;
-    var file = uploadImage(image);
-    console.log(content);
-    add_to_db(content, current_image);
+    // var image = document.getElementById("uploaded_image").value;
+    // var file = uploadImage(image);
+    // console.log(content);
+    // add_to_db(content, current_image);
     //}
 }
 
-async function uploadImage(image){
-    await $.ajax({
-        type: 'POST',
-        dataType: "multipart/form-data",
-        data: image,
-        url: ENV_HOST + 'api/posts/upload_image',
-        success: function(data){
-            return data.filePath;
-        }
-    })
-}
+// async function uploadImage(image){
+//     await $.ajax({
+//         type: 'POST',
+//         dataType: "multipart/form-data",
+//         data: image,
+//         url: ENV_HOST + 'api/posts/upload_image',
+//         success: function(data){
+//             return data.filePath;
+//         }
+//     })
+// }
 
 
-function add_to_db(content, filePath){
+function add_to_db(formdata, content){
 
     var d = new Date();
     var month = d.getMonth() + 1;
-    var date = d.getHours() + ':' + d.getMinutes() + ' ' + d.getDate() + '-' + month  + '-' + d.getFullYear();
+    var mins = d.getMinutes() > 9 ? d.getMinutes() : '0' + d.getMinutes();
+    var hours = d.getHours() > 9 ? d.getHours() : '0' + d.getHours();
+    var date = hours + ':' + mins + ' ' + d.getDate() + '-' + month  + '-' + d.getFullYear();
 
     $.ajax({
         type: 'POST',
-        url: ENV_HOST + '/api/posts/' + content + '/' + date + '/' + filePath,
-        dataType: "json",
+        url: ENV_HOST + '/api/posts/' + content + '/' + date,
+        contentType: false,
+        processData: false,
+        cache: false,
+        data: formdata,
         success: function(data){
             
             //window.alert("Data Inserted");
