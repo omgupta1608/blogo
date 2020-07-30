d4804c6f7322db055084ce4b9d815d2618d2942d
Om Gupta <omgupta1608@gmail.com>
2020-07-30 14:11:16 +0530

    ui changes

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -9,7 +9,7 @@ const ENV_HOST =  'http://'+window.location.hostname+':4000';
             $.each(data, function(index, element) {
                     
                 //var markup = '<h6>' + element.content + '<br> Likes : ' + element.likes_count + '<br> Posted On : ' + element.date_time + '<br><br>';
-                var post = '<div class="card" style="width: 18rem;"><div class="post-body"><h5 class="post-content">' + element.content + '</h5><p class="post-date">' + element.date_time + '</p><a class="btn btn-primary" onclick="Like(this);">Like</a><h5 class="post-likes">' + element.likes_count + '</h5></div></div>';
+                var post = '<div class="card" style="width: 30%; margin-top: 20px; margin-left: 30px"><div class="card-header">'+element.post_id + '</div><div class="post-body"><h5 class="post-content" style="margin-top: 5px;">' + element.content + '</h5><p class="post-date" style="color: gray;">' + element.date_time + '</p><button type="button" class="btn btn-outline-danger">Like</button><h5 class="post-likes">' + element.likes_count + '</h5></div></div>';
                 $('#posts').append(post);
                 //$('#data_table').append($('<tr>').append($('<td>').text(element.password)));
             });
@@ -21,3 +21,12 @@ function Like(ele){
     
 }
 
+{/* <div class="card" style="width: 28rem; margin-top: 20px;">
+    <div class="card-header">'+element.post_id + '</div>
+    <div class="post-body">
+        <h5 class="post-content" style="margin-top: 5px;">' + element.content + '</h5>
+        <p class="post-date" style="color: gray;">' + element.date_time + '</p>
+        <button type="button" class="btn btn-outline-danger">Like</button>
+        <h5 class="post-likes">' + element.likes_count + '</h5>
+    </div>
+</div> */}
